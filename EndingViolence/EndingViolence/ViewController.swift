2e0229b6623475903066ac11ab93714ed1d5b4e8
Steven Thompson <steve.thomp@me.com>
2016-03-05 11:08:26 -0800

    Added xcode project

---
+++ EndingViolence/EndingViolence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EndingViolence
+//
+//  Created by Steven Thompson on 2016-03-05.
+//  Copyright © 2016 teamteamtwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
